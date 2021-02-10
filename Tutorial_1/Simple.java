a0b14f28c8ac419da8c9ca01d0306f301a8637c4
Jordan Allen <jordanallen1332@gmail.com>
2021-02-09 20:21:55 -0500

    Fixed Tutorial_1

--- Tutorial_1/Simple.java
+++ Tutorial_1/Simple.java
@@ -1,5 +1,8 @@
 package Tutorial_1;
 
 public class Simple {
-
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hi, Satelliting!");
+    }
 }
